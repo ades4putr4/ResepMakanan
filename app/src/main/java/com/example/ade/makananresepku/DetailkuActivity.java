265eb61c6c55d9377bbf2667c9c1ed09d5d8f474
ades4putr4 <putra.asa95@gmail.com>
2017-10-12 18:11:03 +0700

    Catatan Penyimpanan

--- app/src/main/java/com/example/ade/makananresepku/DetailkuActivity.java
+++ app/src/main/java/com/example/ade/makananresepku/DetailkuActivity.java
@@ -3,17 +3,24 @@ package com.example.ade.makananresepku;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailkuActivity extends AppCompatActivity {
     ImageView gambar;
+    TextView tv3,tv4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailku);
           Bundle b = getIntent().getExtras();
+        tv3=(TextView)findViewById(R.id.tr);
+        tv4=(TextView)findViewById(R.id.td);
         gambar=(ImageView)findViewById(R.id.gambar);
-         gambar.setImageResource(b.getInt("gambar"));
+        tv3.setText(b.getString("resep"));
+        tv4.setText(b.getString("procedur"));
+        gambar.setImageResource(b.getInt("gambar"));
         setTitle(b.getString("label"));
+
     }
 
 }
